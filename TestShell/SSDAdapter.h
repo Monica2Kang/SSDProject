5d8d840fede8ef45e2821a3a8a361e437d295676
nw82.kang <tomato_zam@naver.com>
2025-05-12 08:28:20 +0900

    [TestShell] SSD Adapter API 생성 및 Test Mock 구현 및 검증 입니다.

--- TestShell/SSDAdapter.h
+++ TestShell/SSDAdapter.h
@@ -1,4 +1,10 @@
 #pragma once
-class SSDAdapter {
 
+#include "ISSDAdapter.h"
+
+class SSDAdapter : public ISSDAdapter{
+	void wirteLba(const int lba, const int data) override;
+	int readLba(const int lba) override;
+	void fullWrite(const int data) override;
+	void fullRead(void) override;
 };
--- TestShell/SSDAdapter_test.cpp
+++ TestShell/SSDAdapter_test.cpp
@@ -5,12 +5,70 @@
 using namespace testing;
 using namespace std;
 
+class MockSSDAdapter : public ISSDAdapter {
+public:
+	MOCK_METHOD(void, wirteLba, (const int lba, const int data), (override));
+	MOCK_METHOD(int, readLba, (const int lba), (override));
+	MOCK_METHOD(void, fullWrite, (const int data), (override));
+	MOCK_METHOD(void, fullRead, (), (override));
+};
+
+
 class SSDAdapterFixture : public Test {
 public:
 	
 };
 
 TEST_F(SSDAdapterFixture, InterfaceTest) {
-	ISSDAdapter instance;
-	EXPECT_NE(&instance, nullptr);
+	ISSDAdapter* ssdAdpater = new SSDAdapter();
+
+	EXPECT_NE(ssdAdpater, nullptr);
+}
+
+TEST_F(SSDAdapterFixture, API_Test_WriteLba) {
+	MockSSDAdapter ssdAdpater;
+
+	const int writeLba = 0;
+	const int data = 0xCAFECAFE;
+
+	EXPECT_CALL(ssdAdpater, wirteLba(writeLba, data))
+		.Times(1);
+
+	ssdAdpater.wirteLba(writeLba, data);
+}
+
+TEST_F(SSDAdapterFixture, API_Test_ReadLba) {
+	MockSSDAdapter ssdAdpater;
+
+	const int readLba = 0;
+	const int data = 0xCAFECAFE;
+
+	EXPECT_CALL(ssdAdpater, readLba(readLba))
+		.Times(1)
+		.WillOnce(Return(data));
+
+	int returnData = ssdAdpater.readLba(readLba);
+	
+	EXPECT_EQ(returnData, data);
+}
+
+TEST_F(SSDAdapterFixture, API_Test_fullWrite) {
+	MockSSDAdapter ssdAdpater;
+
+	const int data = 0xCAFECAFE;
+
+	EXPECT_CALL(ssdAdpater, fullWrite(data))
+		.Times(1);
+
+	ssdAdpater.fullWrite(data);
+}
+
+TEST_F(SSDAdapterFixture, API_Test_fullRead) {
+	MockSSDAdapter ssdAdpater;
+
+
+	EXPECT_CALL(ssdAdpater, fullRead())
+		.Times(1);
+
+	ssdAdpater.fullRead();
 }

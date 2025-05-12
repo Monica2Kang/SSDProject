d1463888e51928afe896e2b4a94baee400c4f2ac
Monica2Kang <aksenwkd92@naver.com>
2025-05-12 09:13:37 +0900

    [SSD] Refactoring : 테스트 코드에서 Fixture 사용하여 중복 코드 제거

--- SSD/SSDCmdParser_test.cpp
+++ SSD/SSDCmdParser_test.cpp
@@ -3,71 +3,79 @@
 #include <string>
 
 using std::string;
+using namespace testing;
 
-TEST(SSDCmdParser, ReadSuccess) {
+class SSDCmdParserFixture : public Test {
+public:
+    int getArgc(const char* argv[]) {
+        return sizeof(argv) / sizeof(argv[0]);
+    }
+};
+
+TEST_F(SSDCmdParserFixture, ReadSuccess) {
     SSDCmdParser parser;
     const char* argv[] = { "SSD.exe", "R", "0" };
-    int argc = sizeof(argv) / sizeof(argv[0]);
+    int argc = getArgc(argv);
 
     string actual = parser.checkParsing(argc, argv);
     string expected = "0x00000000";
     EXPECT_EQ(expected, actual);
 }
 
-TEST(SSDCmdParser, ReadFailInvalidLBA) {
+TEST_F(SSDCmdParserFixture, ReadFailInvalidLBA) {
     SSDCmdParser parser;
     const char* argv[] = { "SSD.exe", "R", "100" };
-    int argc = sizeof(argv) / sizeof(argv[0]);
+    int argc = getArgc(argv);
 
     string actual = parser.checkParsing(argc, argv);
     string expected = "ERROR";
     EXPECT_EQ(expected, actual);
 }
 
-TEST(SSDCmdParser, WriteSuccess) {
+TEST_F(SSDCmdParserFixture, WriteSuccess) {
     SSDCmdParser parser;
     const char* argv[] = { "SSD.exe", "W", "0", "0x12345678" };
-    int argc = sizeof(argv) / sizeof(argv[0]);
+    int argc = getArgc(argv);
 
     string actual = parser.checkParsing(argc, argv);
     string expected = "";
     EXPECT_EQ(expected, actual);
 }
 
-TEST(SSDCmdParser, WriteFailInvalidLBA) {
+TEST_F(SSDCmdParserFixture, WriteFailInvalidLBA) {
     SSDCmdParser parser;
     const char* argv[] = { "SSD.exe", "W", "100", "0x12345678" };
-    int argc = sizeof(argv) / sizeof(argv[0]);
+    int argc = getArgc(argv);
 
     string actual = parser.checkParsing(argc, argv);
     string expected = "ERROR";
     EXPECT_EQ(expected, actual);
 }
 
-TEST(SSDCmdParser, WriteFailDecimalValue) {
+TEST_F(SSDCmdParserFixture, WriteFailDecimalValue) {
     SSDCmdParser parser;
     const char* argv[] = { "SSD.exe", "W", "0", "12345678" };
-    int argc = sizeof(argv) / sizeof(argv[0]);
+    int argc = getArgc(argv);
 
     string actual = parser.checkParsing(argc, argv);
     string expected = "ERROR";
     EXPECT_EQ(expected, actual);
 }
 
-TEST(SSDCmdParser, WriteFailBigValue) {
+TEST_F(SSDCmdParserFixture, WriteFailBigValue) {
     SSDCmdParser parser;
     const char* argv[] = { "SSD.exe", "W", "0", "0x1FFFFFFFF" };
-    int argc = sizeof(argv) / sizeof(argv[0]);
+    int argc = getArgc(argv);
 
     string actual = parser.checkParsing(argc, argv);
     string expected = "ERROR";
     EXPECT_EQ(expected, actual);
 }
 
-TEST(SSDCmdParser, WriteFailSmallValue) {
+TEST_F(SSDCmdParserFixture, WriteFailSmallValue) {
     SSDCmdParser parser;
     const char* argv[] = { "SSD.exe", "W", "0", "0x1" };
-    int argc = sizeof(argv) / sizeof(argv[0]);
+    int argc = getArgc(argv);
 
     string actual = parser.checkParsing(argc, argv);
     string expected = "ERROR";

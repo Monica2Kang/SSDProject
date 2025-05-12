325364fa598ba833ee557891a4a4bc25c37bfa65
Monica2Kang <aksenwkd92@naver.com>
2025-05-12 16:31:58 +0900

    [SSD] SSDCmdParserFixture에서 고정적으로 사용하는 R, W, 0, 100 문자열을 const 값으로 변경

--- SSD/SSDCmdParser_test.cpp
+++ SSD/SSDCmdParser_test.cpp
@@ -9,34 +9,41 @@ using namespace testing;
 class SSDCmdParserFixture : public SSDCmdParser, public Test {
 public:
     SSDCmdParser parser;
-    const char* SSDFileName = "SSD.exe";
+    const char* EXE_FILE_NAME = "SSD.exe";
+    const char* READ_COMMAND = "R";
+    const char* WRITE_COMMAND = "W";
+    const char* DEFAULT_LBA = "0";
+    const char* OVER_LBA = "100";
+    const char* DEFAULT_VALUE = "0x1234ABCD";
 };
 
 TEST_F(SSDCmdParserFixture, InvalidCommand) {
-    const char* argv[] = { SSDFileName };
+    const char* argv[] = { EXE_FILE_NAME };
     int argc = sizeof(argv) / sizeof(argv[0]);
+
     bool actual = parser.checkParsing(argc, argv);
     bool expected = PARSING_FAILED;
     EXPECT_EQ(expected, actual);
 }
 
 TEST_F(SSDCmdParserFixture, ReadSuccess) {
-    const char* argv[] = { SSDFileName, "R", "0" };
+    const char* argv[] = { EXE_FILE_NAME, READ_COMMAND, DEFAULT_LBA };
     int argc = sizeof(argv) / sizeof(argv[0]);
+
     bool actual = parser.checkParsing(argc, argv);
     bool expected = PARSING_SUCCESS;
     EXPECT_EQ(expected, actual);
 }
 
 TEST_F(SSDCmdParserFixture, ReadFailInvalidLBA) {
-    const char* argv[] = { SSDFileName, "R", "100" };
+    const char* argv[] = { EXE_FILE_NAME, READ_COMMAND, OVER_LBA };
     int argc = sizeof(argv) / sizeof(argv[0]);
 
     EXPECT_THROW(parser.checkParsing(argc, argv), std::invalid_argument);
 }
 
 TEST_F(SSDCmdParserFixture, WriteSuccess) {
-    const char* argv[] = { SSDFileName, "W", "0", "0x12345678" };
+    const char* argv[] = { EXE_FILE_NAME, WRITE_COMMAND, DEFAULT_LBA, DEFAULT_VALUE };
     int argc = sizeof(argv) / sizeof(argv[0]);
 
     bool actual = parser.checkParsing(argc, argv);
@@ -45,14 +52,14 @@ TEST_F(SSDCmdParserFixture, WriteSuccess) {
 }
 
 TEST_F(SSDCmdParserFixture, WriteFailInvalidLBA) {
-    const char* argv[] = { SSDFileName, "W", "100", "0x12345678" };
+    const char* argv[] = { EXE_FILE_NAME, WRITE_COMMAND, OVER_LBA, DEFAULT_VALUE };
     int argc = sizeof(argv) / sizeof(argv[0]);
 
     EXPECT_THROW(parser.checkParsing(argc, argv), std::invalid_argument);
 }
 
 TEST_F(SSDCmdParserFixture, WriteFailDecimalValue) {
-    const char* argv[] = { SSDFileName, "W", "0", "12345678" };
+    const char* argv[] = { EXE_FILE_NAME, WRITE_COMMAND, DEFAULT_LBA, "12345678" };
     int argc = sizeof(argv) / sizeof(argv[0]);
 
     bool actual = parser.checkParsing(argc, argv);
@@ -62,7 +69,7 @@ TEST_F(SSDCmdParserFixture, WriteFailDecimalValue) {
 
 TEST_F(SSDCmdParserFixture, WriteFailBigValue) {
 
-    const char* argv[] = { SSDFileName, "W", "0", "0x1FFFFFFFF" };
+    const char* argv[] = { EXE_FILE_NAME, WRITE_COMMAND, DEFAULT_LBA, "0x1FFFFFFFF" };
     int argc = sizeof(argv) / sizeof(argv[0]);
 
     bool actual = parser.checkParsing(argc, argv);
@@ -71,7 +78,7 @@ TEST_F(SSDCmdParserFixture, WriteFailBigValue) {
 }
 
 TEST_F(SSDCmdParserFixture, WriteFailSmallValue) {
-    const char* argv[] = { SSDFileName, "W", "0", "0x1" };
+    const char* argv[] = { EXE_FILE_NAME,WRITE_COMMAND, DEFAULT_LBA, "0x1" };
     int argc = sizeof(argv) / sizeof(argv[0]);
 
     bool actual = parser.checkParsing(argc, argv);

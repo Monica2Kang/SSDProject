529fe27c02af4dd3bbf93dbc2008550818767d8e
Jungyeon Kim <wjddus0904@naver.com>
2025-05-12 14:23:18 +0900

    [TestShell] Red-Green-Refactoring : make write function and parameter validation functions

--- TestShell/shell.cpp
+++ TestShell/shell.cpp
@@ -1,49 +1,162 @@
 #include <iostream>
-#include <string>
+#include <istream>
 #include <sstream>
+#include <vector>
+#include <string>
+#include <cctype>
+#include "shell.h"
 
 using namespace std;
 
-class Shell {
-public:
-	void executeShell(void) {
-		string input;
-		while (input != "exit") {
-			cout << "Shell> ";
-			getline(std::cin, input);
+Shell::Shell(ISSDAdapter* ISSDAdapter) : m_ISSDAdapter(ISSDAdapter)
+{
+}
 
-			stringstream ss(input);
-			string cmd;
-			ss >> cmd;
+void Shell::executeShell(void) {
+	// while(true){
+		// getCommand();
 
-			// command valid check
-			if (cmd != "write" || cmd != "read" || cmd != "exit" || cmd != "help" || cmd != "fullwrite" || cmd != "fullread") {
-				cout << "INVALID COMMAND" << endl;
-				continue;
-			}
+	splitAndStore();
 
-			if (cmd == "write") { // write 3 0xAAAABBBB
-				// command argument valid check
+	if (noEnterCommand()) {
+		cout << "INVALID COMMAND" << endl;
+		return;
+		//continue;
+	}
 
-			}
-			else if (cmd == "read") { // read 2
+	if (parameter[COMMAND_POS] == "write") { // write 3 0xAAAABBBB
+		if (writeApi()) //continue;
+			return;
+	}
+	else if (parameter[COMMAND_POS] == "read") { // read 2
+		if (readApi()) //continue;
+			return;
+	}
+	else if (parameter[COMMAND_POS] == "exit") { // exit
+		if (exitApi()) //break;
+			return;
+	}
+	else if (parameter[COMMAND_POS] == "help") { // help AmazingReviewer jungyeonKim
+		if (helpApi()) //continue;
+			return;
+	}
+	else if (parameter[COMMAND_POS] == "fullwrite") { // fullwrite 0xAAAABBBB
+		if (fullwriteApi()) //continue;
+			return;
+	}
+	else if (parameter[COMMAND_POS] == "fullread") { // fullread 
+		if (fullreadApi()) //continue;
+			return;
+	}
+	cout << "INVALID COMMAND" << endl;
+	//}
+}
 
-			}
-			else if (cmd == "exit") { // exit
-				break;
-			}
-			else if (cmd == "help") { // help AmazingReviewer jungyeonKim
+void Shell::getCommand(string command)
+{
+	cout << "Shell> ";
+	input = command;
+	// getline(std::cin, input);
+}
 
-			}
-			else if (cmd == "fullwrite") { // fullwrite 0xAAAABBBB
+void Shell::splitAndStore(void) {
+	parameter.clear();
+	stringstream ss(input);
+	string word;
 
-			}
-			else if (cmd == "fullread") { // fullread 
+	while (ss >> word) {
+		parameter.push_back(word);
+	}
+}
 
-			}
-			else {
-				cout << "INVALID COMMAND" << endl;
+bool Shell::noEnterCommand(void) {
+	if (parameter.size() < 1 || parameter[0].empty()) {
+		return true;
+	}
+	return false;
+}
+
+bool Shell::writeApi(void)
+{
+	if (isValidParameterSize(WRITE_PARAMETER_SIZE)) {
+		if (isValidLBA(LBA_POS)) {
+			storeLBA();
+			if (isValidData(DATA_POS)) {
+				storeData(DATA_POS);
+				m_ISSDAdapter->wirteLba(LBA, data);
+				cout << "[Write] Done\n" << endl;
+				return true;
 			}
 		}
 	}
-};
+	return false;
+}
+
+bool Shell::readApi(void){
+	return true;
+}
+
+bool Shell::exitApi(void) {
+	return true;
+}
+
+bool Shell::helpApi(void) {
+	return true;
+}
+
+bool Shell::fullwriteApi(void){
+	return true;
+}
+
+bool Shell::fullreadApi(void){
+	return true;
+}
+
+bool Shell::isValidParameterSize(int size) {
+	if (parameter.size() == size) {
+		return true;
+	}
+	return false;
+}
+
+bool Shell::isValidLBA(int pos) {
+	if (parameter[pos].empty() || parameter[pos].length() > 2) {
+		return false;
+	}
+
+	for (char ch : parameter[pos]) {
+		if (!isdigit(ch)) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+bool Shell::isValidData(int pos) {
+	const string str = parameter[pos];
+
+	if (str.empty() || str.length() != DATA_LENGTH) {
+		return false;
+	}
+
+	if (!(str[0] == '0' && str[1] == 'x')) {
+		return false;
+	}
+
+	for (int i = 2; i < DATA_LENGTH; i++) {
+		if (!((str[i] >= 'A' && str[i] <= 'F') || (str[i] >= '0' && str[i] <= '9'))) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+void Shell::storeLBA(void) {
+	LBA = stoi(parameter[LBA_POS]);
+}
+
+void Shell::storeData(int pos) {
+	data = static_cast<int>(stoul(parameter[pos], nullptr, 16));
+}
--- TestShell/shell.h
+++ TestShell/shell.h
@@ -1,5 +1,43 @@
 #pragma once
+#include "ISSDAdapter.h"
+#include <vector>
+#include <string>
+
 class Shell {
 public:
+	const int DATA_LENGTH = 10;
+	const int COMMAND_POS = 0;
+	const int LBA_POS = 1;
+	const int DATA_POS = 2;
+	const int FULLWRITE_DATA_POS = 1;
+	const int WRITE_PARAMETER_SIZE = 3;
+	const int READ_PARAMETER_SIZE = 2;
+	const int EXIT_PARAMETER_SIZE = 1;
+	const int HELP_PARAMETER_SIZE = 3;
+	const int FULLWRITE_PARAMETER_SIZE = 2;
+	const int FULLREAD_PARAMETER_SIZE = 1;
+
+	Shell(ISSDAdapter* ISSDAdapter);
 	void executeShell(void);
+	void getCommand(std::string command);
+	void splitAndStore(void);
+	bool noEnterCommand(void);
+	bool writeApi(void);
+	bool readApi(void);
+	bool exitApi(void);
+	bool helpApi(void);
+	bool fullwriteApi(void);
+	bool fullreadApi(void);
+	bool isValidParameterSize(int size);
+	bool isValidLBA(int pos);
+	bool isValidData(int pos);
+	void storeLBA(void);
+	void storeData(int pos);
+
+protected:
+	ISSDAdapter* m_ISSDAdapter;
+	std::string input;
+	std::vector<std::string> parameter;
+	int LBA;
+	int data;
 };

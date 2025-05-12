02fa0c9a760ece35242a9596d805985056a5bd6d
Janghwan.K <gaihwan@gmail.com>
2025-05-12 09:53:47 +0900

    [SSD] Red - Green : writeData() 에 대한 invalid Argument TC 추가

--- SSD/SSDDevice.cpp
+++ SSD/SSDDevice.cpp
@@ -5,9 +5,11 @@ int SSDDevice::readData(int lba) {
     return 0;
 }
 
+void SSDDevice::writeData(int lba, const int data) {
+    if (isLbaOutOfRange(lba)) throw std::invalid_argument("Out of LBA Ragne");
+}
+
 bool SSDDevice::isLbaOutOfRange(const int lba) const {
     return LBA_UPPER_LIMIT < lba || lba < LBA_LOWER_LIMIT;
 }
 
-void SSDDevice::writeData(int LBA, int data) {
-}
--- SSD/SSDDevice_test.cpp
+++ SSD/SSDDevice_test.cpp
@@ -8,12 +8,21 @@ using namespace std;
 class SSDDeviceFixture : public Test {
 public:
     SSDDevice ssd;
-    void validArgumentTest(const int lba) {
-        EXPECT_NO_THROW(ssd.readData(lba), invalid_argument);
+    void validArgumentTest4readData(const int lba) {
+        EXPECT_NO_THROW(ssd.readData(lba));
     }
-    void invalidArgumentTest(const int lba) {
+    void invalidArgumentTest4readData(const int lba) {
         EXPECT_THROW(ssd.readData(lba), invalid_argument);
     }
+    void validArgumentTest4writeData(int lba) {
+        EXPECT_NO_THROW(ssd.writeData(lba, SAMPLE_DATA));
+    }
+    void invalidArgumentTest4writeData(int lba) {
+        EXPECT_THROW(ssd.writeData(lba, SAMPLE_DATA), invalid_argument);
+    }
+
+private:
+    static const int SAMPLE_DATA = static_cast<int>(0x1082);
 };
 
 TEST_F(SSDDeviceFixture, ssdCreationTC) {
@@ -28,20 +37,41 @@ TEST_F(SSDDeviceFixture, ssdReadDataTC) {
 TEST_F(SSDDeviceFixture, ssdReadDataTC4LBAInRange) {
     vector<int> lba = { 0, 1, 10, 20, 30, 53, 75, 97, 98, 99 };
     for (int addr : lba) {
-        validArgumentTest(addr);
+        validArgumentTest4readData(addr);
     }
 }
 
 TEST_F(SSDDeviceFixture, ssdReadDataTC4LBAOutOfRange) {
     vector<int> lba = { 100, 101, 110, 253, 337, 1553, 25675 };
     for (int addr : lba) {
-        invalidArgumentTest(addr);
+        invalidArgumentTest4readData(addr);
     }
 }
 
 TEST_F(SSDDeviceFixture, ssdReadDataTC4LBAOutOfRangeMinus) {
     vector<int> lba = { -1, -2, -110, -253,-1082, -337, -4 };
     for (int addr : lba) {
-        invalidArgumentTest(addr);
+        invalidArgumentTest4readData(addr);
+    }
+}
+
+TEST_F(SSDDeviceFixture, ssdWriteDataTC_LBAInRange) {
+    vector<int> lba = { 0, 1, 10, 20, 30, 53, 75, 97, 98, 99 };
+    for (int addr : lba) {
+        validArgumentTest4writeData(addr);
+    }
+}
+
+TEST_F(SSDDeviceFixture, ssdWriteDataTC_LBAOutOfRange) {
+    vector<int> lba = { 100, 101, 110, 253, 337, 1553, 25675 };
+    for (int addr : lba) {
+        invalidArgumentTest4writeData(addr);
+    }
+}
+
+TEST_F(SSDDeviceFixture, ssdWriteDataTC_LBAOutOfRangeMinus) {
+    vector<int> lba = { -1, -2, -110, -253,-1082, -337, -4 };
+    for (int addr : lba) {
+        invalidArgumentTest4writeData(addr);
     }
 }

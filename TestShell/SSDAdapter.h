810dc3ed8ea80ce37358cfbc97c50b495a836c99
nw82.kang <tomato_zam@naver.com>
2025-05-12 09:57:14 +0900

    [TestShell] Write 기능 및 TC 추가

--- TestShell/SSDAdapter.h
+++ TestShell/SSDAdapter.h
@@ -1,10 +1,14 @@
 #pragma once
 
 #include "ISSDAdapter.h"
-
+#include <string>
 class SSDAdapter : public ISSDAdapter{
+public:
 	void wirteLba(const int lba, const int data) override;
 	int readLba(const int lba) override;
 	void fullWrite(const int data) override;
 	void fullRead(void) override;
+
+private:
+	void _ExecuteSSDCommand(std::string command);
 };

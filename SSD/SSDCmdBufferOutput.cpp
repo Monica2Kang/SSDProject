25a785e232657906b35747c36d436a64b373c0bc
Monica2Kang <aksenwkd92@naver.com>
2025-05-13 16:47:16 +0900

    [SSD] SSDCmdBufferOutput private 함수 추가

--- SSD/SSDCmdBufferOutput.cpp
+++ SSD/SSDCmdBufferOutput.cpp
@@ -1,36 +1,68 @@
-#ifdef _WIN32
-#include <direct.h>
-#define mkdir(path) _mkdir(path)
-#else
-#include <sys/stat.h>
-#include <sys/types.h>
-#endif
+﻿#include <windows.h>
+#include <shlwapi.h>  // PathFileExists
+#include <direct.h>   // _mkdir
+#include <vector>
 #include "SSDCmdBufferOutput.h"
+
+using namespace std;
 
 SSDCmdBufferOutput::SSDCmdBufferOutput() {
     // file open with overwrite mode
-    if (mkdir(folderName) != 0) {
-        throw std::runtime_error("Cannot create the folder");
-    }
 
-    for (int index = 0; index < MAX_FILE_COUNT; index++) {
-        std::string fileName = std::string(folderName) + "/" + std::to_string(index) + "_" + fileEmptyName;
-        outfile.open(fileName, std::ios::out);
-        if (!outfile.is_open()) {
-            throw std::runtime_error("Cannot open the buffer txt file");
-        }
-    }
 }
 
 SSDCmdBufferOutput::~SSDCmdBufferOutput() {
-    for (int index = 0; index < MAX_FILE_COUNT; index++) {
-        std::string fileName = std::string(folderName) + "/" + std::to_string(index) + "_" + fileEmptyName;        
-        if (outfile.is_open()) {
-            outfile.close();
+
+}
+
+bool SSDCmdBufferOutput::_folderExists(void) {
+    struct stat info;
+    return (stat(folderName, &info) == 0 && (info.st_mode & S_IFDIR));
+}
+
+void SSDCmdBufferOutput::_createFolder(void) {
+    if (!_folderExists()) {
+        if (mkdir(folderName) != 0) {
+            throw runtime_error("Cannot create the buffer folder");
         }
+    }
+    else {
+        throw runtime_error("buffer folder is already created");
     }
 }
 
-//SSDCmdBufferOutput::clearFolder(void) {
-//
-//}
+void SSDCmdBufferOutput::_clearFilesInFolder(void) {
+    string searchPath = string(folderName) + "\\*";
+    WIN32_FIND_DATAA findData;
+    HANDLE hFind = FindFirstFileA(searchPath.c_str(), &findData);
+
+    if (hFind == INVALID_HANDLE_VALUE) {
+        throw runtime_error("buffer folder is already empty");
+        return;
+    }
+
+    do {
+        string fileName = findData.cFileName;
+        if (fileName == "." || fileName == "..") continue;
+
+        string filePath = string(folderName) + "\\" + fileName;
+        if (!DeleteFileA(filePath.c_str())) {
+            throw runtime_error("Cannot delete file");
+        }
+    } while (FindNextFileA(hFind, &findData));
+    FindClose(hFind);
+}
+
+void SSDCmdBufferOutput::_createFilesInFolder(void) {
+    string fileName = "";
+    string filePath = string(folderName) + "\\" + fileName;
+    ofstream file(filePath);
+    if (file.is_open()) {
+        file.close();
+    }
+    else {
+        throw runtime_error("Cannot create file");
+    }
+
+}
+
--- SSD/SSDCmdBufferOutput.h
+++ SSD/SSDCmdBufferOutput.h
@@ -10,6 +10,11 @@ public:
     ~SSDCmdBufferOutput();
     
 private:
+    bool _folderExists(void);
+    void _createFolder(void);
+    void _clearFilesInFolder(void);
+    void _createFilesInFolder(void);
+
     std::ofstream outfile;
     const char* folderName = "buffer";
     const std::string& fileEmptyName = "empty.txt";

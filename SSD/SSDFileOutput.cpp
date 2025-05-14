c93971cfd9b2d1fed7042a56c255e3de4df47766
Janghwan.K <gaihwan@gmail.com>
2025-05-14 19:59:11 +0900

    [SSD] Refactoring : Bug Fix - SSDFileOutput 클래스 file creation 오류 수정

--- SSD/SSDFileOutput.cpp
+++ SSD/SSDFileOutput.cpp
@@ -2,9 +2,12 @@
 
 SSDFileOutput::SSDFileOutput() {
     // file open with overwrite mode
-    outfile.open(filename, std::ios::out);
+    std::ofstream outfile(filename, std::ios::out);
     if (!outfile.is_open()) {
         throw std::runtime_error("Cannot open the Log file");
+    }
+    if (outfile.is_open()) {
+        outfile.close();
     }
 }
 
@@ -15,14 +18,35 @@ SSDFileOutput::~SSDFileOutput() {
 }
 
 void SSDFileOutput::printData(const unsigned int value) {
+    outfile.open(filename, std::ios::out);
+    if (!outfile.is_open()) {
+        throw std::runtime_error("Cannot open the Log file");
+    }
     outfile << "0x" << std::uppercase << std::setfill('0') << std::setw(OUTPUT_DIGIT)
         << std::hex << value << std::endl;
+    if (outfile.is_open()) {
+        outfile.close();
+    }
 }
 
 void SSDFileOutput::printError(void) {
+    outfile.open(filename, std::ios::out);
+    if (!outfile.is_open()) {
+        throw std::runtime_error("Cannot open the Log file");
+    }
     outfile << "ERROR" << std::endl;
+    if (outfile.is_open()) {
+        outfile.close();
+    }
 }
 
 void SSDFileOutput::printDone(void) {
+    outfile.open(filename, std::ios::out);
+    if (!outfile.is_open()) {
+        throw std::runtime_error("Cannot open the Log file");
+    }
     outfile << "" << std::endl;
+    if (outfile.is_open()) {
+        outfile.close();
+    }
 }
